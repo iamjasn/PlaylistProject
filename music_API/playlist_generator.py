ea3b13921436663145e9d88a8837dfc0095498d9
Jason K. <iamjasn@users.noreply.github.com>
2014-09-30 00:43:28 -0700

    Update playlist_generator.py

--- music_API/playlist_generator.py
+++ music_API/playlist_generator.py
@@ -8,9 +8,6 @@ from api_keys import songkick_api, rdio, echo_nest_api_start, echo_nest_api_end
 Songkick and Rdio."""
 
 
-user_country = 'US'
-
-
 
 # def truncate_to_300(text):
 #     if len(text) <= 300:
