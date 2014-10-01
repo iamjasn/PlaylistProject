895c6e8968c20ff8d2bf0fd06e1f1696d0ffef67
iamjasn <jkuhafkan@gmail.com>
2014-09-30 17:46:00 -0700

    delete file

--- PlaylistProject/tests/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-from music_API.playlist_generator import Show, get_upcoming_shows, get_tracks
-
-class ShowTestCase(TestCase):
-    def setUp(self):
-        Show.objects.create
